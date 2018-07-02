f45545645dedf3380d3887c24b1a2e6a289ca133
Michael Buslik <michaelbuslik@hotmail.com>
2018-07-01 22:41:52 -0700

    More background stuff but needs more work.

--- Scripts/Parallax.js
+++ Scripts/Parallax.js
@@ -1,11 +1,16 @@
 function ParallaxHandler( gfx )
 {
-	const img = gfx.LoadImage( "Images/Background.png" );
+	const img1 = gfx.LoadImage( "Images/Background1.png" );
+	const img2 = gfx.LoadImage( "Images/Background2.png" );
 	// 
 	this.Draw=( moveAmount,gfx )=>
 	{
-		gfx.DrawImage( img,
+		return;
+		gfx.DrawImage( img1,
 			Vec2( moveAmount.x * 0.02,moveAmount.y * 0.01 ),
-			gfx.ScreenSize );
+			gfx.ScreenSize.GetMultiplied( 2 ) );
+		gfx.DrawImage( img2,
+			Vec2( moveAmount.x * 1.2,moveAmount.y * 0.01 ),
+			Vec2( gfx.ScreenWidth * 4,gfx.ScreenHeight * 2 ) );
 	}
 }
